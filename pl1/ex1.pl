a362068ca2bafd05970758887c1225fedbe653c0
afonsoafonsoafonso <afonsomendonca91@gmail.com>
2019-09-23 01:09:39 +0100

    ex pl1 feitos em casa

---
+++ pl1/ex1.pl
@@ -0,0 +1,23 @@
+male(aldo).
+male(lincoln).
+male(michael).
+male(lj).
+
+female(christina).
+female(lisa).
+female(sara).
+female(ella).
+
+parent(christina,lincoln).
+parent(christina,michael).
+parent(aldo,lincoln).
+parent(aldo,michael).
+
+parent(lisa, lj).
+parent(lincoln, lj).
+
+parent(michael, ella).
+parent(sara, ella).
+
+% a) parent(X,michael). (; e 'enter' até ter todas as respostas)
+% b) parent(aldo,X). idem 
