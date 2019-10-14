8982bb430950677b920ca61cca7f946fb323fb42
afonsoafonsoafonso <afonsomendonca91@gmail.com>
2019-10-14 12:48:58 +0100

    ex6 b) e c) feito

--- pl3/ex6.pl
+++ pl3/ex6.pl
@@ -2,15 +2,21 @@ myappend([], L, L).
 myappend([X|L1], L2, [X|L3]):- 
     myappend(L1, L2, L3).
 
+% ALÍNEA A)
 delete_one(X, L, L1):-
     myappend(La, [X|Lb], L),
     myappend(La, Lb, L1).
 
-% não funciona bem bem bem. continua para sempre
-% o processo de eliminação???
-delete_all(X, L, L).
+% ALÍNEA B)
+delete_all(X, L, L):-
+    \+ member(X,L).
 delete_all(X, L1, L2):-
     delete_one(X, L1, L),
     delete_all(X, L, L2).
 
 
+% ALÍNEA C)
+delete_all_list([], L, L).
+delete_all_list([X|LX], L1, L2):-
+    delete_all(X, L1, L3),
+    delete_all_list(LX, L3, L2).
