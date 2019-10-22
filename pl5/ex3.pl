de7037da3849018398c67185bdc31b636da8709c
afonsoafonsoafonso <afonsomendonca91@gmail.com>
2019-10-22 01:54:33 +0100

    plt ex2 and ex3

---
+++ pl5/ex3.pl
@@ -0,0 +1,16 @@
+% ALÍNEA A)
+% R: um
+%    dois
+%    tres
+%    ultima_clausula
+%    no/false
+
+% ALÍNEA B)
+% R: um
+%    no/false
+
+% ALÍNEA C)
+% R: um-um
+%    um-dois
+%    um-tres
+%    no
