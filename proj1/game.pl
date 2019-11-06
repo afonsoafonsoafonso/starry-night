0fe6e958603bbd1c6da93c63de2af202e2127716
FilipeCarlosNogueira <fcadcn@gmail.com>
2019-11-06 15:52:04 +0000

    validate_piece

--- proj1/game.pl
+++ proj1/game.pl
@@ -19,20 +19,27 @@ start_game:-
 
 game_loop(B, P):-
     display_game(B, P),
-    % TO-DO: verificar se há vencedor
-    % pede por um move do user
+    /* 
+    * TO-DO: verificar se há vencedor
+    * pede por um move do user
+    */
+
+    /*
+    select_piece(X, Y),
+    get_move(X_increment, Y_increment),
+    */
+    
     get_move(X1, Y1, X2, Y2),
     move(X1, Y1, X2, Y2, B, B1),
-    display_game(B1, P),
-    game_loop(B1, P).
-
+    Pnext is P+1,
+    game_loop(B1, Pnext).
 
 get_move(X1, Y1, X2, Y2):-
     nl,
     write('X1: \n'),
     read(X1),
     nl,
-    write('Y1: \n'),
+    write('Y2: \n'),
     read(Y1),
     nl,
     write('X2: \n'),
@@ -42,15 +49,54 @@ get_move(X1, Y1, X2, Y2):-
     read(Y2),
     nl.
 
+/*
+get_move(X_increment, Y_increment) :-
+    nl,
+    write('MOVES: W(up), S(down), D(right), A(left)'), nl.
+    read().
+
+select_piece(X, Y) :-
+    nl,
+    write('X: '),
+    read(X),
+    nl,
+    write('Y: '),
+    read(Y),
+    nl.
+*/
+
+/*
+* 
+*/
 move(X1, Y1, X2, Y2, Board, NewBoard):-
     get_cell(X1, Y1, Board, C1),
+    validate_piece(C1),
     get_cell(X2, Y2, Board, C2),
+    validate_blank_space(C2),
     change_cell(X1, Y1, Board, AuxBoard, C2),
     change_cell(X2, Y2, AuxBoard, NewBoard, C1).
+
+move(X1, Y1, X2, Y2, Board, NewBoard):-
+    get_move(X1, Y1, X2, Y2),
+    move(X1, Y1, X2, Y2, Board, NewBoard).
 
 get_cell(X, Y, Board, CellValue):-
     nth0(X, Board, AuxRow),
     nth0(Y, AuxRow, CellValue).
+
+/*
+* Verifies if the coords choosen contains a valid piece.
+*/
+validate_piece(C1) :-
+    write('valid piece\n') :- C1 >= 0.
+
+validate_piece(C1) :- write('invalid piece\n').
+
+/*
+* Verifies if the coords choosen contains a blank space.
+*/
+validate_blank_space(C2) :-
+    C2 =:= 0.
 
 change_cell(X, Y, Board, NewBoard, CellValue):-
     get_to_row(X, Y, CellValue, Board, NewBoard).
