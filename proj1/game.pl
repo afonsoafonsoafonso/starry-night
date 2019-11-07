a2ea4fa4c6f929b73d3680ca2d4347361f043d7b
afonsoafonsoafonso <afonsomendonca91@gmail.com>
2019-11-07 00:49:30 +0000

    fixed bug when you couldn re-enter new coordinates when last ones were invalid. also some refactoring

--- proj1/game.pl
+++ proj1/game.pl
@@ -18,17 +18,7 @@ start_game:-
     game_loop(B, P).
 
 game_loop(B, P):-
-    display_game(B, P),
-    /* 
-    * TO-DO: verificar se há vencedor
-    * pede por um move do user
-    */
-
-    /*
-    select_piece(X, Y),
-    get_move(X_increment, Y_increment),
-    */
-    
+    display_game(B, P),    
     get_move(X1, Y1, X2, Y2),
     move(X1, Y1, X2, Y2, B, B1),
     Pnext is P+1,
@@ -49,34 +39,15 @@ get_move(X1, Y1, X2, Y2):-
     read(Y2),
     nl.
 
-/*
-get_move(X_increment, Y_increment) :-
-    nl,
-    write('MOVES: W(up), S(down), D(right), A(left)'), nl.
-    read().
-
-select_piece(X, Y) :-
-    nl,
-    write('X: '),
-    read(X),
-    nl,
-    write('Y: '),
-    read(Y),
-    nl.
-*/
-
-/*
-* 
-*/
 move(X1, Y1, X2, Y2, Board, NewBoard):-
     get_cell(X1, Y1, Board, C1),
-    validate_piece(C1),
+    cell_with_ship(C1),
     get_cell(X2, Y2, Board, C2),
-    validate_blank_space(C2),
+    empty_cell(C2),
     change_cell(X1, Y1, Board, AuxBoard, C2),
     change_cell(X2, Y2, AuxBoard, NewBoard, C1).
 
-move(X1, Y1, X2, Y2, Board, NewBoard):-
+move(_, _, _, _, Board, NewBoard):-
     get_move(X1, Y1, X2, Y2),
     move(X1, Y1, X2, Y2, Board, NewBoard).
 
@@ -87,16 +58,17 @@ get_cell(X, Y, Board, CellValue):-
 /*
 * Verifies if the coords choosen contains a valid piece.
 */
-validate_piece(C1) :-
-    write('valid piece\n') :- C1 >= 0.
+cell_with_ship(C):-
+    C>0,
+    write('cell with ship\n').
 
-validate_piece(C1) :- write('invalid piece\n').
+% depois pode-se apagar (é só para testing purposes)
+cell_with_ship(_):- 
+    write('invalid cell\n').
 
-/*
-* Verifies if the coords choosen contains a blank space.
-*/
-validate_blank_space(C2) :-
-    C2 =:= 0.
+% checks if given cell is empty
+empty_cell(C):-
+    C=:=0.
 
 change_cell(X, Y, Board, NewBoard, CellValue):-
     get_to_row(X, Y, CellValue, Board, NewBoard).
