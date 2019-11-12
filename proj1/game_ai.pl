3a7d9921146a6451975981b7d9afc5ac2076a0fb
afonsoafonsoafonso <afonsomendonca91@gmail.com>
2019-11-12 02:19:28 +0000

    puta que pariu est amerda ta corrigida esta piçada fdp

--- proj1/game_ai.pl
+++ proj1/game_ai.pl
@@ -1,1 +1,19 @@
+game_loop(B, P, 1):-
+    end_game_A(B),
+    write('PLAYER A WON').
 
+game_loop(B, P, 1):-
+    end_game_B(B),
+    write('PLAYER B WON').
+
+game_loop(B, P, 1):-
+    display_game(B, P),
+    ( P =:= 1 ->
+      move(B, B1, P),
+      game_loop(B1, 2)
+    ; cpu_move(B, B1, P),
+      game_loop(B1, 1)
+    ).
+
+cpu_move(Board, NewBoard, P):-
+    P =:= 2.
