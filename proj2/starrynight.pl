ea4c9b73854169f3a5cf40556f3a08fe7bf9a8d2
afonsoafonsoafonso <afonsomendonca91@gmail.com>
2019-12-16 13:31:14 +0000

    adicionei cenas das restriçoes laterais mas n sei porque nao as respeita na integra

--- proj2/starrynight.pl
+++ proj2/starrynight.pl
@@ -16,6 +16,9 @@ midBoard([
     [_, _, _, _, _]
     ]).
 
+midBoardRestrictionsRows([3, 0, 2, 0, 1]).
+midBoardRestrictionsCols([0, 0, 0, 0, 0]).
+
 bigBoard([
     [_, _, _, _, _, _],
     [_, _, _, _, _, _],
@@ -33,11 +36,13 @@ bigBoard([
 */
 
 starrynight(B):-
-    bigBoard(B),
+    midBoard(B),
+    midBoardRestrictionsCols(RestrictCols),
+    midBoardRestrictionsRows(RestrictRows),
     create_board_domains(B),
-    check_lines(B),
+    check_lines(B, RestrictRows),
     transpose(B, TB),
-    check_lines(TB),
+    check_lines(TB, RestrictCols),
     append(B, FB),
     labeling([], FB).
 
@@ -46,15 +51,45 @@ create_board_domains([H|T]):-
     domain(H, 0, 3),
     create_board_domains(T).
 
-check_lines([]).
-check_lines([H|T]):-
+check_lines([], []).
+check_lines([H|T], [RH|RT]):-
     list_sum(H, S),
     S#=6,
     count(1, H, #=, 1),
     count(2, H, #=, 1),
     count(3, H, #=, 1),
-    check_lines(T).
+    enforce_side_restriction(H, RH),
+    check_lines(T, RT).
 
+enforce_side_restriction(_, 0).
+
+enforce_side_restriction(L, 1):-
+    element(SunPos, L, 1),
+    element(MoonPos, L, 2),
+    element(StarPos, L, 3),
+    DeltaSun #= abs(StarPos - SunPos),
+    DeltaMoon #= abs(StarPos - MoonPos),
+
+    DeltaSun #< DeltaMoon.
+
+enforce_side_restriction(L, 2):-
+    element(SunPos, L, 1),
+    element(MoonPos, L, 2),
+    element(StarPos, L, 3),
+    DeltaSun #= abs(StarPos - SunPos),
+    DeltaMoon #= abs(StarPos - MoonPos),
+
+    DeltaSun #> DeltaMoon.
+
+enforce_side_restriction(L, 3):-
+    element(SunPos, L, 1),
+    element(MoonPos, L, 2),
+    element(StarPos, L, 3),
+    DeltaSun #= abs(StarPos - SunPos),
+    DeltaMoon #= abs(StarPos - MoonPos),
+
+    DeltaSun #= DeltaMoon.
+    
 list_sum([H|T], Value):-
     listSumAux([H|T], 0, Value).
 
