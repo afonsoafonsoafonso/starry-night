0d5443a1898a185db3d991cab07c3d49f8550316
Afonso <afonsomendonca91@gmail.com>
2019-12-31 14:21:33 +0000

    diagonal restrictions implemented but not working. labeling doesnt find any solutions (but they exist)

--- proj2/starrynight.pl
+++ proj2/starrynight.pl
@@ -6,7 +6,7 @@ smallBoard([
     [_, _, _, _],
     [_, _, _, _],
     [_, _, _, _]
-    ]).
+    ], 4).
 
 midBoard([
     [_, _, _, _, _],
@@ -14,7 +14,7 @@ midBoard([
     [_, _, _, _, _],
     [_, _, _, _, _],
     [_, _, _, _, _]
-    ]).
+    ], 5).
 
 midBoardRestrictionsRows([0, 0, 0, 3, 0]).
 midBoardRestrictionsCols([2, 0, 0, 3, 0]).
@@ -26,7 +26,7 @@ bigBoard([
     [_, _, _, _, _, _],
     [_, _, _, _, _, _],
     [_, _, _, _, _, _]
-    ]).
+    ], 6).
 
 bigBoardRestrictionsRows([1, 2, 1, 2, 2, 1]).
 bigBoardRestrictionsCols([2, 2, 1, 2, 1, 2]).
@@ -39,11 +39,12 @@ bigBoardRestrictionsCols([2, 2, 1, 2, 1, 2]).
 */
 
 starrynight(B):-
-    midBoard(B),
+    midBoard(B, N),
     midBoardRestrictionsCols(RestrictCols),
     midBoardRestrictionsRows(RestrictRows),
     create_board_domains(B),
     check_lines(B, RestrictRows),
+    enforce_diagonal_restrictions(B, B, N, 1),
     transpose(B, TB),
     check_lines(TB, RestrictCols),
     append(TB, FTB),
@@ -91,6 +92,46 @@ enforce_side_restriction(L, 3):-
 
     DeltaSun #= DeltaMoon.
 
+enforce_diagonal_restrictions(_, _, NRows, NRows).
+enforce_diagonal_restrictions([H|T], Board, NRows, CurrRow):-
+    check_row_diagonals(H, Board, NRows, CurrRow, 1),
+    NextRow is CurrRow + 1,
+    enforce_diagonal_restrictions(T, Board, NRows, NextRow).
+
+check_row_diagonals([H|T], Board, NRows, CurrRowN, 1):-
+    BelowRowN is CurrRowN + 1,
+    RightCol is 2,
+    %element(BelowRowN, Board, BelowRow),
+    nth1(BelowRowN, Board, BelowRow),
+    element(RightCol, BelowRow, RightDiagonal),
+    H #\= RightDiagonal,
+    write('First Col of Row Finished'), nl,
+    check_row_diagonals(T, Board, NRows, CurrRowN, RightCol).
+
+check_row_diagonals([H|T], Board, NRows, CurrRowN, NRows):-
+    CurrColN is NRows,
+    BelowRowN is CurrRowN + 1,
+    LeftCol is CurrColN - 1,
+    %element(BelowRowN, Board, BelowRow),
+    nth1(BelowRowN, Board, BelowRow),
+    element(LeftCol, BelowRow, LeftDiagonal),
+    write('Last Col of Row Finished'), nl,
+    H #\= LeftDiagonal.
+    %check_row_diagonals(T, Board, NRows, CurrRowN, RightCol).
+
+check_row_diagonals([H|T], Board, NRows, CurrRowN, CurrColN):-
+    BelowRowN is CurrRowN + 1,
+    LeftCol is CurrColN - 1,
+    RightCol is CurrColN + 1,
+    %element(BelowRowN, Board, BelowRow)
+    nth1(BelowRowN, Board, BelowRow),
+    element(LeftCol, BelowRow, LeftDiagonal),
+    element(RightCol, BelowRow, RightDiagonal),
+    H #\= RightDiagonal,
+    H #\= LeftDiagonal,
+    write('Col of Row Finished'), nl,
+    check_row_diagonals(T, Board, NRows, CurrRowN, RightCol).
+    
 % display functions
 
 display_solution([], [], RC):-
