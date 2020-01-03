3d7eabdc9047c9f0282af25cbe0a527f44f55061
Afonso <afonsomendonca91@gmail.com>
2020-01-03 04:18:11 +0000

    get n inputs

--- proj2/utils.pl
+++ proj2/utils.pl
@@ -12,8 +12,18 @@ user_input(Input, Min, Max):-
     nl,
     user_input(Input, Min, Max).
 
+get_n_inputs(Min, Max, N, R):-
+    get_n_inputs_aux(Min, Max, N, [], R).
+
+get_n_inputs_aux(_, _, 0, RF, RF).
+get_n_inputs_aux(Min, Max, N, R, RF):-
+    user_input(Input, Min, Max),
+    N1 is N - 1,
+    get_n_inputs_aux(Min, Max, N1, [Input|R], RF).
+/*
 get_n_inputs(_, _, 0, R).
 get_n_inputs(Min, Max, N, [Input|R]):-
     user_input(Input, Min, Max),
     N1 is N - 1,
     get_n_inputs(Min, Max, N1, R).
+*/
